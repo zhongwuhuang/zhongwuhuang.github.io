a1f0b4bd31d9b5e81078173069d309879a5052c8
zhongwuhuang <15280100634@139.com>
2020-07-17 11:56:17 +0800

    首页

--- smarth/static/js/common.js
+++ smarth/static/js/common.js
@@ -1,28 +1,30 @@
 $(function () {
 
     // 引入头部导航
-    $.ajaxSetup({ cache: false });
+    // $.ajaxSetup({ cache: false });
     $('header').load("./components/header.html", initHead);
     $('footer').load("./components/footer.html", initFoot);
+    // 头部背景显示隐藏
     function initHead() {
-        // 头部背景显示隐藏
         function showHeader() {
             if ($(window).scrollTop() >= 380) {
                 $('.header').css('background', 'rgba(255, 255, 255, 0.9)');
                 $(".nav_a").css("color", "#333");
+                $(".navbar-default").css("borderBottom", "1px solid #edf0f2");
             } else {
                 $('.header').css('background', 'none');
                 $(".nav_a").css("color", "#fff");
+                $(".navbar-default").css("borderBottom", "1px solid rgba(255,255,255,.2)");
             }
         }
         showHeader()
-
         $(window).scroll(function () {
             showHeader()
         });
     }
+
+    // 快速置顶
     function initFoot() {
-        // 快速置顶
         function showBtn() {
             if ($(window).scrollTop() >= 200) {
                 // $('#updown').fadeIn(300);
@@ -37,11 +39,8 @@ $(function () {
             showBtn()
         });
         $('#updown').click(function () {
-            console.log(2343);
             $('html,body').animate({ scrollTop: '0px' }, 300);
         });
     }
-
-
 
 });
